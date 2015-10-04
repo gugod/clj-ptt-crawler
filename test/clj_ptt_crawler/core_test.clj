aa9c1185118ec9372173bbbb94e85280246d388f
Kang-min Liu <gugod@gugod.org>
2015-10-04 18:15:03 +0200

    init

---
+++ test/clj_ptt_crawler/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-ptt-crawler.core-test
+  (:require [clojure.test :refer :all]
+            [clj-ptt-crawler.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
