a49bc1594ba98bc91b6a0aa4f4a017fe3460d7cf
Kang-min Liu <gugod@gugod.org>
2015-10-06 22:54:58 +0200

    implement harvest-articles

--- src/clj_ptt_crawler/crawl_board.clj
+++ src/clj_ptt_crawler/crawl_board.clj
@@ -18,5 +18,19 @@
        (map (fn [it] {:url it, :page_number (->> it (re-find #"\d+") Integer/parseInt) }))
        ))
 
+(defn harvest-articles
+  [url board-name]
+  (->> (-> url
+           (client/get {:insecure? true})
+           (:body)
+           (enlive/html-snippet)
+           (enlive/select [[:a (enlive/attr-starts :href (.concat "/bbs/" board-name)) (enlive/attr-ends :href ".html")]]))
+       (filter #(not (.contains (:href (:attrs %)) "/index")))
+       (map (fn [it]
+              (let [url (:href (:attrs it))
+                    id  (->> url (re-find (re-pattern (str/join ["/bbs/" board-name "/(.+)\\.html"]))) second)]
+                {:subject (:content it), :url url, :id id })))
+       ))
+
 (defn -main [board_name output_dir]
   (println (harvest-board-indices (clojure.string/join "" [PTT_URL "/bbs/" board_name "/index.html"]) board_name)))
