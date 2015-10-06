4a89cbe83736b713bbb5bca882015c3c2070fdc9
Kang-min Liu <gugod@gugod.org>
2015-10-07 00:33:24 +0200

    implement download-articles

--- src/clj_ptt_crawler/crawl_board.clj
+++ src/clj_ptt_crawler/crawl_board.clj
@@ -15,7 +15,7 @@
            (enlive/select [[:a (enlive/attr-contains :href "/bbs/") (enlive/attr-contains :href "/index")]]))
        (map #(:href (:attrs %)))
        (filter #(not (.endsWith % "/index.html")))
-       (map (fn [it] {:url it, :page_number (->> it (re-find #"\d+") Integer/parseInt) }))
+       (map (fn [it] {:url (.concat PTT_URL it), :page_number (->> it (re-find #"\d+") Integer/parseInt) }))
        ))
 
 (defn harvest-articles
@@ -27,10 +27,19 @@
            (enlive/select [[:a (enlive/attr-starts :href (.concat "/bbs/" board-name)) (enlive/attr-ends :href ".html")]]))
        (filter #(not (.contains (:href (:attrs %)) "/index")))
        (map (fn [it]
-              (let [url (:href (:attrs it))
-                    id  (->> url (re-find (re-pattern (str/join ["/bbs/" board-name "/(.+)\\.html"]))) second)]
-                {:subject (:content it), :url url, :id id })))
-       ))
+              (let [href (:href (:attrs it))
+                    id  (->> href (re-find (re-pattern (str/join ["/bbs/" board-name "/(.+)\\.html"]))) second)]
+                {:subject (:content it), :url (.concat PTT_URL href), :id id })))))
+
+(defn download-articles [articles board-name output-dir]
+  (doseq [a articles]
+    (let [output-file-name (str/join [output-dir "/" board-name "/" (:id a) ".html"])]
+      (println "==> " output-file-name)
+      (clojure.java.io/make-parents output-file-name)
+      (spit output-file-name (:body (client/get (:url a) {:insecure? true}))))))
 
 (defn -main [board_name output_dir]
-  (println (harvest-board-indices (clojure.string/join "" [PTT_URL "/bbs/" board_name "/index.html"]) board_name)))
+  (doseq [x (->> (-> (clojure.string/join "" [PTT_URL "/bbs/" board_name "/index.html"])
+                     (harvest-board-indices board_name))
+                 (map (fn [it] (harvest-articles (:url it) board_name))))]
+    (download-articles x board_name output_dir)))
