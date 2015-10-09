359d8e5c4c63058256a305efd377a31ebde7b0b8
Kang-min Liu <gugod@gugod.org>
2015-10-09 09:07:22 +0200

    simplified with "at"

--- src/clj_ptt_crawler/convert_articles_html.clj
+++ src/clj_ptt_crawler/convert_articles_html.clj
@@ -6,23 +6,15 @@
 (defn html-only [f] (and (.isFile f) (re-find #"\.html$" (.getName f))))
 
 (defn convert-this [f]
-  (let [output-file-name (.replace (.getPath f) ".html" ".json")]
-    (prn "==> " output-file-name)
-    (-> f
-        slurp
-        enlive/html-snippet
-        ((fn [dom]  {:dom (enlive/select dom [[:div (enlive/attr-contains :id "main-content")]])}))
-        ((fn [article]
-           ;; (enlive/select (:dom article) [[:div (enlive/attr-contains :class "article-metaline")]])
-           {:meta [ (enlive/select (:dom article) [:div.article-metaline] ) ],
-            :push [ (enlive/select (:dom article) [:div.push ] ) ],
-            :body (enlive/select (:dom article) [:div#main-content])}
-           ))
-        prn
-        ;;   (for [x (enlive/select dom [[:div (enlive/attr-contains :class "article-metaline")]])]
-        ;;     [(enlive/select x [[:span (enlive/attr-contains :class "article-meta-tag")]])
-        ;;      (enlive/select x [[:span (enlive/attr-contains :class "article-meta-value")]])]))
-        )))
+  (let [output-file-name (.replace (.getPath f) ".html" ".json")
+        dom (enlive/html-snippet (slurp f))]
+    (prn
+     "==> " output-file-name
+     (enlive/at
+      (enlive/select dom [:div#main-content])
+      [:div.article-metaline] (enlive/content)
+      [:div.push] (enlive/content)
+      ))))
 
 (defn -main [ptt_dir]
   (let [files (filter html-only (file-seq (clojure.java.io/file ptt_dir)))]
