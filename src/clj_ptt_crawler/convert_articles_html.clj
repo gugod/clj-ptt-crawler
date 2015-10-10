3a833f4efeb2177d498a75fb80f80a0782c9a33f
Kang-min Liu <gugod@gugod.org>
2015-10-10 08:59:35 +0200

    split the function to extract "push" and "article-meta".

--- src/clj_ptt_crawler/convert_articles_html.clj
+++ src/clj_ptt_crawler/convert_articles_html.clj
@@ -5,16 +5,35 @@
 
 (defn html-only [f] (and (.isFile f) (re-find #"\.html$" (.getName f))))
 
+(defn extract-text-content [dom sel]
+  (first (enlive/select dom [sel :> enlive/text-node ])))
+
+(defn extract-meta-tag-value [dom]
+  (->> (enlive/select dom [:div.article-metaline])
+      (map
+       (fn [mt]
+         [(extract-text-content mt :span.article-meta-tag),
+          (extract-text-content mt :span.article-meta-value)]))))
+
+(defn extract-push [dom]
+  (->> (enlive/select dom [:div.push])
+       (map
+        (fn [pt] {
+                  :tag        (extract-text-content pt :span.push-tag),
+                  :userid     (extract-text-content pt :span.push-userid),
+                  :content    (extract-text-content pt :span.push-cotnent),
+                  :ipdatetime (extract-text-content pt :span.push-ipdatetime),
+                  }))))
+
+(defn extract-article [dom]
+  {:meta (extract-meta-tag-value dom),
+   :push (extract-push dom),
+   :body ""})
+
 (defn convert-this [f]
   (let [output-file-name (.replace (.getPath f) ".html" ".json")
-        dom (enlive/html-snippet (slurp f))]
-    (prn
-     "==> " output-file-name
-     (enlive/at
-      (enlive/select dom [:div#main-content])
-      [:div.article-metaline] (enlive/content)
-      [:div.push] (enlive/content)
-      ))))
+        article-dom (enlive/select (enlive/html-snippet (slurp f)) [:div#main-content])]
+    (prn "==> " output-file-name (extract-article article-dom))))
 
 (defn -main [ptt_dir]
   (let [files (filter html-only (file-seq (clojure.java.io/file ptt_dir)))]
